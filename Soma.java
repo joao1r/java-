256341aa76bd4dd25655668747bb03dfab1c4e44
joao1r <32169203+joao1r@users.noreply.github.com>
2017-11-24 16:11:26 +0000

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,15 @@
+
+public class Soma {
+import java.util.Scanner;
+	public static void main(String[] args) {
+		Scanner L = new Scanner (System.in);
+	int soma=0;
+	int num;
+	System.out.print("Qual o numero a introduzir?");
+	num = l.nextInt();		
+	for(int I=1;I<=10;I++)
+		soma+=I;
+	System.out.println(I);
+	}
+    System.out.println(soma);
+}

34c4dacb25744518409fe174e952e933fd94d58b
joao1r <32169203+joao1r@users.noreply.github.com>
2017-11-24 15:09:20 +0000

    Add files via upload

---
+++ BemVindoAoJava.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class BemVindoAoJava {
+
+	public static void main(String[] args) {
+String mensagem = "Hoje é sexta feira";
+for (int I=1; I<=5;I++)
+	System.out.println(mensagem);
+
+
+	}
+
+}

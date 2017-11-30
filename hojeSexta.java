02a250ac3f04244367e12d46e55861e88f13f7fc
joao1r <32169203+joao1r@users.noreply.github.com>
2017-11-30 17:17:06 +0000

    Add files via upload

---
+++ hojeSexta.java
@@ -0,0 +1,12 @@
+package hojeSexta;
+
+public class hojeSexta {
+
+	public static void main(String[] args) {
+		String mensagem = "Hoje é Sexta-Feira!";
+		for (int I=1; I<=5; I++)
+			System.out.println(mensagem);
+
+	}
+
+}
